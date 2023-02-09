38e5c561dfc3f72a52ef70e94cf74b72a7f2e904
CSmith <layaway-0idol@icloud.com>
2023-02-08 21:20:24 -0500

    commit tree f945ef50035568390d4a9f05a19a546216354985

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {[...cats,cats.push("Ralph")]};
+function destructivelyPrependCat(name) {cats.unshift("Bob")};
+function destructivelyRemoveLastCat() {cats.pop()};
+function destructivelyRemoveFirstCat() {cats.shift()};
+function removeFirstCat() {[cats.slice(1)]};
